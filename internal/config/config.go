197be336829b8a3c6e4eab70d31e5acb91aa9a59
Aleksandr Vinogradov <vin89441@gmail.com>
2025-06-08 16:06:36 +0300

    config load

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env           string `yaml:"env"`
+	ConfigServer  `yaml:"server"`
+	ConfigMongoDB `yaml:"mongodb"`
+}
+
+type ConfigServer struct {
+	Port         string        `yaml:"port"`
+	ReadTimeout  time.Duration `yaml:"read_timeout"`
+	WriteTimeout time.Duration `yaml:"write_timeout"`
+	IdleTimeout  time.Duration `yaml:"idle_timeout"`
+}
+
+type ConfigMongoDB struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	DBName   string `yaml:"db_name"`
+	CollName string `yaml:"coll_name"`
+}
+
+func Load() *Config {
+	cfg := new(Config)
+
+	if err := cleanenv.ReadConfig("config.yaml", cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	return cfg
+}
