06f5137dd212ace6a8070eec5e1e4713dee9b7ea
Aleksandr Vinogradov <vin89441@gmail.com>
2025-06-12 18:03:55 +0300

    init logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "production"
+)
+
+func New(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
