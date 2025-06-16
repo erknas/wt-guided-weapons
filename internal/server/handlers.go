36b0e0f7bda6545b871ec47f91123bbb774031c8
Aleksandr Vinogradov <vin89441@gmail.com>
2025-06-16 18:05:10 +0300

    fix return

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -6,9 +6,9 @@ import (
 	"github.com/erknas/wt-guided-weapons/internal/types"
 )
 
-func (s *Server) hadleInsertWeapon(w http.ResponseWriter, r *http.Request) error {
+func (s *Server) handleInsertWeapon(w http.ResponseWriter, r *http.Request) error {
 	if err := s.svc.InsertWeapons(r.Context()); err != nil {
-		return writeJSON(w, http.StatusBadRequest, err)
+		return err
 	}
 
 	return writeJSON(w, http.StatusOK, nil)
@@ -17,7 +17,7 @@ func (s *Server) hadleInsertWeapon(w http.ResponseWriter, r *http.Request) error
 func (s *Server) handleGetWeapons(w http.ResponseWriter, r *http.Request) error {
 	weapons, err := s.svc.GetWeapons(r.Context())
 	if err != nil {
-		return writeJSON(w, http.StatusBadRequest, err)
+		return err
 	}
 
 	return writeJSON(w, http.StatusOK, types.Weapons{Weapons: weapons})
