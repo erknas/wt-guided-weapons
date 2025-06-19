98f8de33d1081b80d13dcfc269481417de19c201
Aleksandr Vinogradov <vin89441@gmail.com>
2025-06-19 15:44:28 +0300

    update logs

--- cmd/main.go
+++ cmd/main.go
@@ -25,14 +25,18 @@ func main() {
 	}
 	defer logger.Sync()
 
-	logger.Info("config loaded", zap.Any("cfg", cfg))
+	logger.Info("config loaded",
+		zap.Any("cfg", cfg),
+	)
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
 	storage, err := storage.New(ctx, cfg)
 	if err != nil {
-		logger.Error("failed to init storage", zap.Error(err))
+		logger.Error("failed to init storage",
+			zap.Error(err),
+		)
 		os.Exit(1)
 	}
 
@@ -43,7 +47,9 @@ func main() {
 		defer cancel()
 
 		if err := storage.Close(closeCtx); err != nil {
-			logger.Warn("failed to disconnect from storage", zap.Error(err))
+			logger.Warn("failed to disconnect from storage",
+				zap.Error(err),
+			)
 		}
 
 		logger.Info("storage closed")
@@ -51,7 +57,14 @@ func main() {
 
 	service := service.New(storage, storage, logger)
 
-	server := server.New(service, logger)
+	server, err := server.New(service, logger)
+	if err != nil {
+		logger.Error("failed to initialize server",
+			zap.Error(err),
+		)
+		os.Exit(1)
+	}
+
 	if err := server.Run(ctx, cfg); err != nil {
 		logger.Error("server failed", zap.Error(err))
 		os.Exit(1)
