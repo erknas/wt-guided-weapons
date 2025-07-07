e99eb81cb0446f56187f40197048dd97633b12d4
Aleksandr Vinogradov <vin89441@gmail.com>
2025-07-07 20:40:54 +0300

    CategoSelector component

---
+++ client/src/components/CategorySelector.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { WEAPONS_CATEGORIES } from '../consts/weaponsCategories';
+
+defineProps({
+	modelValue: String
+})
+
+defineEmits(['update:modelValue'])
+
+const categories = WEAPONS_CATEGORIES
+</script>
+
+<template>
+	<select :value="modelValue" @change="$emit('update:modelValue', $event.target.value)">
+		<option v-for="category in categories" :value="category.value" :key="category.value">
+			{{ category.label }}
+		</option>
+	</select>
+</template>
