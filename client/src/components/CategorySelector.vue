f92badc68185862176913238db5293f2649b83f6
Aleksandr Vinogradov <vin89441@gmail.com>
2025-07-10 16:51:36 +0300

    changes

--- client/src/components/CategorySelector.vue
+++ client/src/components/CategorySelector.vue
@@ -11,9 +11,26 @@ const categories = WEAPONS_CATEGORIES
 </script>
 
 <template>
-	<select :value="modelValue" @change="$emit('update:modelValue', $event.target.value)">
-		<option v-for="category in categories" :value="category.value" :key="category.value">
-			{{ category.label }}
-		</option>
-	</select>
+	<div class="selector-container">
+		<select :value="modelValue" @change="$emit('update:modelValue', $event.target.value)" class="category-selector">
+			<option v-for="category in categories" :value="category.value" :key="category.value" :class="`option-${category.value}`">
+				{{ category.label }}
+			</option>
+		</select>
+	</div>
 </template>
+
+<style scoped>
+.selector-container {
+	position: fixed;
+	top: 20px;
+	left: 0px;
+	z-index: 1000;
+}
+
+.category-selector {
+	min-width: 170px;
+	padding: 8px;
+	font-size: 15px;
+}
+</style>
