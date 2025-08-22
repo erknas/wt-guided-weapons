ceb413967c4be4703dddaf521cf5c97b5ac13cf0
Aleksandr Vinogradov <vin89441@gmail.com>
2025-08-22 16:57:48 +0300

    change methods name, add Version() and UpsertVersion

--- internal/storage/mongodb/mongodb.go
+++ internal/storage/mongodb/mongodb.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"regexp"
 	"time"
@@ -11,6 +12,7 @@ import (
 	"github.com/erknas/wt-guided-weapons/internal/types"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.uber.org/zap"
 )
 
@@ -18,6 +20,8 @@ const (
 	FieldWeaponID        = "id"
 	FieldWeaponsCategory = "category"
 	FieldWeaponName      = "name"
+	FieldVersionID       = "_id"
+	CurrentVersion       = "current_version"
 )
 
 type MongoDB struct {
@@ -48,7 +52,7 @@ func New(ctx context.Context, cfg *config.Config) (*MongoDB, error) {
 	}, nil
 }
 
-func (m *MongoDB) Upsert(ctx context.Context, weapons []*types.Weapon) error {
+func (m *MongoDB) UpsertWeapons(ctx context.Context, weapons []*types.Weapon) error {
 	log := logger.FromContext(ctx, logger.Storage)
 
 	models := make([]mongo.WriteModel, 0, len(weapons))
@@ -59,10 +63,11 @@ func (m *MongoDB) Upsert(ctx context.Context, weapons []*types.Weapon) error {
 		}
 
 		filter := bson.M{FieldWeaponID: weapon.ID}
+		update := updateWeapon(weapon)
 
 		model := mongo.NewUpdateOneModel()
 		model.SetFilter(filter)
-		model.SetUpdate(updateWeapon(weapon))
+		model.SetUpdate(update)
 		model.SetUpsert(true)
 
 		models = append(models, model)
@@ -76,7 +81,7 @@ func (m *MongoDB) Upsert(ctx context.Context, weapons []*types.Weapon) error {
 		return fmt.Errorf("failed to upsert documents: %w", err)
 	}
 
-	log.Debug("Upsert complited",
+	log.Debug("UpsertWeapons complited",
 		zap.Int("matched count", int(res.MatchedCount)),
 		zap.Int("upserted count", int(res.UpsertedCount)),
 		zap.Int("modified count", int(res.ModifiedCount)),
@@ -85,7 +90,7 @@ func (m *MongoDB) Upsert(ctx context.Context, weapons []*types.Weapon) error {
 	return nil
 }
 
-func (m *MongoDB) ByCategory(ctx context.Context, category string) ([]*types.Weapon, error) {
+func (m *MongoDB) WeaponsByCategory(ctx context.Context, category string) ([]*types.Weapon, error) {
 	log := logger.FromContext(ctx, logger.Storage)
 
 	filter := bson.M{FieldWeaponsCategory: category}
@@ -112,17 +117,17 @@ func (m *MongoDB) ByCategory(ctx context.Context, category string) ([]*types.Wea
 		log.Error("Cursor error",
 			zap.Error(err),
 		)
-		return nil, fmt.Errorf("cursor error: %w", err)
+		return nil, fmt.Errorf("last cursor error: %w", err)
 	}
 
-	log.Debug("ByCategory complited",
+	log.Debug("WeaponsByCategory complited",
 		zap.Int("total documents found", len(weapons)),
 	)
 
 	return weapons, nil
 }
 
-func (m *MongoDB) Search(ctx context.Context, query string) ([]types.SearchResult, error) {
+func (m *MongoDB) WeaponsByName(ctx context.Context, query string) ([]types.SearchResult, error) {
 	log := logger.FromContext(ctx, logger.Storage)
 
 	filter := bson.M{
@@ -154,14 +159,67 @@ func (m *MongoDB) Search(ctx context.Context, query string) ([]types.SearchResul
 		log.Error("Cursor error",
 			zap.Error(err),
 		)
-		return nil, fmt.Errorf("cursor error: %w", err)
+		return nil, fmt.Errorf("last cursor error: %w", err)
 	}
 
-	log.Debug("Search complited",
+	log.Debug("WeaponsByName complited",
 		zap.Int("total documents found", len(results)),
 	)
 
 	return results, nil
+}
+
+func (m *MongoDB) Version(ctx context.Context) (types.LastChange, error) {
+	log := logger.FromContext(ctx, logger.Storage)
+
+	filter := bson.M{FieldVersionID: CurrentVersion}
+
+	var version types.LastChange
+
+	err := m.coll.FindOne(ctx, filter).Decode(&version)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			log.Error("Version not found",
+				zap.Error(err),
+			)
+			return types.LastChange{}, fmt.Errorf("version not found: %w", err)
+		}
+		log.Error("Decode error",
+			zap.Error(err),
+		)
+		return types.LastChange{}, fmt.Errorf("failed to find document: %w", err)
+	}
+
+	log.Debug("Version complited",
+		zap.Any("found document", version),
+	)
+
+	return version, nil
+}
+
+func (m *MongoDB) UpsertVersion(ctx context.Context, version types.VersionInfo) error {
+	log := logger.FromContext(ctx, logger.Storage)
+
+	opts := options.UpdateOne().SetUpsert(true)
+
+	filter := bson.M{FieldVersionID: CurrentVersion}
+	update := updateVersion(version)
+
+	res, err := m.coll.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		log.Error("Update error",
+			zap.Error(err),
+		)
+		return fmt.Errorf("failed to update document: %w", err)
+	}
+
+	log.Debug("UpsetVersion complited",
+		zap.Int("matched count", int(res.MatchedCount)),
+		zap.Int("upserted count", int(res.UpsertedCount)),
+		zap.Int("modified count", int(res.ModifiedCount)),
+	)
+
+	return nil
 }
 
 func (m *MongoDB) Close(ctx context.Context) error {
